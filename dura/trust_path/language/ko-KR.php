0669030ac2860cc26c8842d76e70083cced7b814
suinyeze <suinyeze@16d95a31-15d7-1725-fbc0-e82009b4db45>
2010-04-03 21:14:47 +0000

    韓国語翻訳を追加
    git-svn-id: http://drrr-like-chat.googlecode.com/svn/trunk@17 16d95a31-15d7-1725-fbc0-e82009b4db45

---
+++ dura/trust_path/language/ko-KR.php
@@ -0,0 +1,40 @@
+<?php
+
+function dura_catalog()
+{
+	return array(
+		"Please input name." => "이름을 입력해 주십시오.",
+		"Name should be less than 10 letters." => "이름은 10글짜 이내로 입력해 주십시오.",
+		"Member should be more than {1}." => "인수는 {1} 명 이상으로 해 주십시오.",
+		"Member should be less than {1}." => "인수는 {1} 명 이하로 해 주십시오.",
+		"Cannot create new room any more." => "더 이상 방을 만들 수 없습니다.",
+		"{1} logged in." => "ーー {1}님이 로그인 하셨습니다.",
+		"{1} logged out." => "ーー {1}님이 로그아웃 하셨ㅅ브니다.",
+		"{1} lost the connection." => "ーー {1}님의 접속이 끊겼습니다.",
+		"Data Error: Room creating failed." => "오류: 방 작성 실패",
+		"Room Name" => "방 이름",
+		"Max Members" => "정원",
+		"{1} members" => "{1} 명",
+		"Up to {1} rooms can be created." => "*방은 최대로 {1} 실까지 만들 수 있습니다.",
+		"Create Room" => "방 만들기",
+		'If auto reload doesn\'t work,  please click <a href="{1}">here</a>.' => '자동 전송이 안 될 경우 <a href="{1}">여기</a>를 클릭해 주십시오.',
+		"LOGOUT" => "LOGOUT",
+		"CREATE ROOM" => "방 만들기",
+		"Lounge" => "로비",
+		"full" => "만월",
+		"LOGIN" => "LOGIN",
+		"Login error happened." => "외부에서 로그인을 시도했다 등의 이유로 인해 오류가 발생했습니다.",
+		"ENTER" => "ENTER",
+		"Room not found." => "방이 없거나 삭제되었습니다.",
+		"Room is full." => "방이 만원이 되었습니다.",
+		"{1} users online!" => "{1} 명이 채팅중!",
+		"Admin" => "관리",
+		"Admin ID" => "관리자ID",
+		"Password" => "비밀변호",
+		"ID or password is wrong." => "ID나 비밀번호가 옳지 않습니다.",
+		"Announce" => "전체 아나운스",
+		"Admin Announce" => "관리자 아나운스",
+	);
+}
+
+?>
